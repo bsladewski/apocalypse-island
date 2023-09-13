d914d0cd2b91f97f42922c16ee839cfa6fc8eaf6
Ben Sladewski <bsladewski@gmail.com>
2023-09-12 20:33:13 -0400

    Fixed animation speed and pathfinding bug.

--- Assets/Scripts/Survivor/SurvivorAnimationController.cs
+++ Assets/Scripts/Survivor/SurvivorAnimationController.cs
@@ -1,5 +1,6 @@
 using Pathfinding;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SurvivorAnimation : MonoBehaviour
 {
@@ -9,8 +10,18 @@ public class SurvivorAnimation : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
+    private Vector3 lastPosition;
+
+    private void Awake()
+    {
+        lastPosition = transform.position;
+    }
+
     private void Update()
     {
-        animator.SetFloat("Velocity", aiPath.desiredVelocity.magnitude / aiPath.maxSpeed);
+        Vector3 moveVector = new Vector3(lastPosition.x - transform.position.x, 0f,
+            lastPosition.z - transform.position.z);
+        animator.SetFloat("Velocity", moveVector.magnitude / Time.deltaTime / aiPath.maxSpeed);
+        lastPosition = transform.position;
     }
 }
